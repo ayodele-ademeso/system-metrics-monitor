8ceb0a6e59c49585317c0b54ba55870d9590d771
Ayodele <ademesoayodele@gmail.com>
2024-01-03 10:41:20 +0000

    refactor: added a function that writes output to the metrics.txt file

--- metrics_monitor.sh
+++ metrics_monitor.sh
@@ -10,44 +10,54 @@ set -e
 
 METRICS_FILE="metrics.txt"
 
+# Function to write metrics to the file
+write_to_metrics(){
+    echo "$@" >> "$METRICS_FILE"
+}
+
 #function to get cpu metrics
 cpu_metrics(){
-    top -b -n 1 | grep '%Cpu' >> $METRICS_FILE
+    top -b -n 1 | grep '%Cpu'
 }
 
 #function to get memory metrics
 memory_metrics(){
-    free -mh >> $METRICS_FILE
+    free -mh
 }
 
 #function to get load average
 load_average(){
-    top -b -n 1 | grep "load average:" | awk '{print $11, $12, $13, $14, $15}' >> $METRICS_FILE
+    top -b -n 1 | grep "load average:" | awk '{print $11, $12, $13, $14, $15}'
 }
 
 #function to get disk space information
 disk_usage(){
-    df -h | awk 'NR==1; $NF == "/" {print}' >> $METRICS_FILE
+    df -h | awk 'NR==1; $NF == "/" {print}'
 }
 
 while true; do
     DATE=$(date)
-    echo "   Metrics For $DATE   " >> $METRICS_FILE
-    echo "" >> $METRICS_FILE
-    echo "......Cpu Metrics......." >> $METRICS_FILE
-    cpu_metrics
-    echo "" >> $METRICS_FILE
-    echo ".....Load Average......" >> $METRICS_FILE
-    load_average
-    echo "" >> $METRICS_FILE
-    echo ".....Memory Metrics....." >> $METRICS_FILE
-    memory_metrics
-    echo "" >> $METRICS_FILE
-    echo ".....Disk Usage....." >> $METRICS_FILE
-    disk_usage
-    echo "" >> $METRICS_FILE
+    write_to_metrics "   Metrics For $DATE   "
+    write_to_metrics ""
+
+    write_to_metrics "......Cpu Metrics......."
+    write_to_metrics "$(cpu_metrics)"
+
+    write_to_metrics ""
+    write_to_metrics ".....Load Average......"
+    write_to_metrics "$(load_average)"
+
+    write_to_metrics ""
+    write_to_metrics ".....Memory Metrics....."
+    write_to_metrics "$(memory_metrics)"
+
+    write_to_metrics ""
+    write_to_metrics ".....Disk Usage....."
+    write_to_metrics "$(disk_usage)"
+
+    write_to_metrics ""
     sleep 60
 done
 
 #set thresholds
-# create a single function that does the writing to metrics file
+#create a single function that does the writing to metrics file
